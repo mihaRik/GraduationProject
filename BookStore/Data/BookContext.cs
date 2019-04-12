839ffc3b9ed98220369d13eab46a7b695eb552fb
Togrul Mammadli <mamedlitogrul99@gmail.com>
2019-04-12 11:46:24 +0400

    initial create

---
+++ BookStore/Data/BookContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+
+namespace BookStore.Data
+{
+    public class BookContext : IdentityDbContext
+    {
+        public BookContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Book>().HasData();
+        }
+    }
+}
