135974a54fa3fc2490ac9bc323e5e594ffcc9413
Rahim Teymurov <rahimrt@code.edu.az>
2019-05-06 23:02:57 +0400

    add to favorites function begin to write

--- BookStore/wwwroot/js/site.js
+++ BookStore/wwwroot/js/site.js
@@ -1,9 +1,17 @@
-﻿
-$(document).ready(function () {
+﻿$(document).ready(function () {
     $(document).on("click", "#favorites", function () {
-        $('.modal')
-            .modal('')
-            .modal('setting', 'transition', 'vertical flip')
-            .modal('show');
+        $("#favorites-container").show();
+        $("#favorites-container").css("transform", "rotateX(0)");
+        $("#favorites-container").addClass("active");
+    })
+
+    $(document).click(function (e) {
+        if ($(e.target) != $("#favorites-container")) {
+            if (!$("#favorites-container").hasClass("active")) {
+                $("#favorites-container").removeClass("active");
+                $("#favorites-container").hide();
+                $("#favorites-container").css("transform", "rotateX(-85deg)");
+            }
+        }
     })
 })
